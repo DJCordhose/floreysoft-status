64416c4c7d8f617176bab524e95b1b8e9c4d9961
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2016-08-09 13:26:39 +0200

    more complex example

--- src/main/js/src/components/Overview.js
+++ src/main/js/src/components/Overview.js
@@ -1,10 +1,72 @@
 /* @flow */
 
-import React from 'react';
+import React, {Component} from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 
-const Overview = () => (
-    <RaisedButton label="Default" />
-);
+const styles = {
+    container: {
+        textAlign: 'center',
+        paddingTop: 200,
+    },
+};
+
+type Props = any;
+
+type State = {
+    open: boolean;
+    text: string;
+};
+
+class Overview extends Component<any, Props, State> {
+    state: State;
+
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            open: false,
+            text: ""
+        };
+    }
+
+    render() {
+        const {open} = this.state;
+
+        const standardActions = (
+            <FlatButton
+                label="Ok"
+                primary={true}
+                onTouchTap={() => this.setState({open: false})}
+            />
+        );
+
+        return (
+            <div style={styles.container}>
+                <Dialog
+                    open={open}
+                    title="Sekrete"
+                    actions={standardActions}
+                    onRequestClose={() => this.setState({open: false})}
+                >
+                    <TextField
+                        hintText="Fucking text"
+                        floatingLabelText="Enter the fucking text"
+                        onChange={(e) => this.setState({text: e.target.value})}
+                        onKeyDown={(e: KeyboardEvent)=> e.keyCode === 13 && this.setState({open: false})}
+                    />
+                </Dialog>
+                <h1>Material-UI</h1>
+                <h2>example project</h2>
+                <RaisedButton
+                    label="Add URL"
+                    secondary={true}
+                    onTouchTap={() => this.setState({open: true})}
+                />
+            </div>
+        );
+    }
+}
 
 export default Overview;
--- src/main/js/src/components/main.js
+++ src/main/js/src/components/main.js
@@ -2,11 +2,19 @@
 
 import React from 'react';
 
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {deepOrange500} from 'material-ui/styles/colors';
 import Overview from './Overview';
 
+const muiTheme = getMuiTheme({
+    palette: {
+        accent1Color: deepOrange500,
+    },
+});
+
 const App = () => (
-    <MuiThemeProvider>
+    <MuiThemeProvider muiTheme={muiTheme}>
         <Overview />
     </MuiThemeProvider>
 );
