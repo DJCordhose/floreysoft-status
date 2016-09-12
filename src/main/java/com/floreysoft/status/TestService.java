93eca75f07c329e7240640df6c63cf580136dbc4
Daniel Florey <daniel.florey@gmail.com>
2016-09-12 16:36:47 +0200

    urlfetchservice

--- src/main/java/com/floreysoft/status/TestService.java
+++ src/main/java/com/floreysoft/status/TestService.java
@@ -1,21 +1,26 @@
 package com.floreysoft.status;
 
-import com.floreysoft.status.dto.Report;
-import com.floreysoft.status.dto.Test;
-import com.floreysoft.status.entity.Status;
-import com.floreysoft.status.entity.TestEntity;
-import com.floreysoft.status.entity.TestRunEntity;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.KeyFactory;
-import com.google.appengine.api.datastore.Query;
-
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.floreysoft.status.dto.Report;
+import com.floreysoft.status.dto.Test;
+import com.floreysoft.status.entity.Status;
+import com.floreysoft.status.entity.TestEntity;
+import com.floreysoft.status.entity.TestRunEntity;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.urlfetch.HTTPResponse;
+import com.google.appengine.api.urlfetch.URLFetchService;
+import com.google.appengine.api.urlfetch.URLFetchServiceFactory;
+
 public class TestService {
     private static final Logger logger = Logger.getLogger(TestService.class.getName());
+    private static final URLFetchService urlFetchService = URLFetchServiceFactory.getURLFetchService();
     private TestDao testDao = new TestDao();
 
     public void sampleTests() {
@@ -28,12 +33,15 @@ public class TestService {
         }
     }
 
-    public Status executeSampleRun(TestEntity test) {
+    public Status executeSampleRun(TestEntity test) throws MalformedURLException, IOException {
         final String url = test.getUrl();
         logger.log(Level.INFO, "Running test against: " + url);
         final String goldenMaster = test.getGoldenMaster();
         if (goldenMaster == null || goldenMaster.length() == 0) {
             logger.log(Level.WARNING, "Test not initialized for: " + url);
+            HTTPResponse response = urlFetchService.fetch(new URL(url));
+            byte[] content = response.getContent();
+            test.setGoldenMaster(content);
             // FIXME: commented out for testing only
 //            return Status.UNINITIALIZED;
         }
